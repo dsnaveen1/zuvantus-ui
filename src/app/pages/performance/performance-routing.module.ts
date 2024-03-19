43a95615f9e1c45299eb938ece194cf0c6ba8bfc
dcrakesh-24 <rakeshmudhaliyar@gmail.com>
2024-03-19 15:07:50 +0530

    added performance tables

---
+++ src/app/pages/performance/performance-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AllEmployeeComponent } from './all-employees/all-employee.component';
+import { PerformanceComponent } from './performance.component';
+import { ProbationEmployeeComponent } from './probation-employees/probation-employee.component';
+import { ConfirmedEmployeeComponent } from './confirmed-employees/confirmed-employee.component';
+import { ExitedEmployeeComponent } from './exited-employees/exited-employee.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: PerformanceComponent,
+    children: [
+      {
+        path: 'allemployee',
+        component: AllEmployeeComponent,
+      },
+      {
+        path: 'probationemployee',
+        component: ProbationEmployeeComponent,
+      },
+      {
+        path: 'confirmedemployee',
+        component: ConfirmedEmployeeComponent,
+      },
+      {
+        path: 'exitedemployee',
+        component: ExitedEmployeeComponent,
+      },
+      
+      { path: '', redirectTo: 'allemployee', pathMatch: 'full' },
+      { path: '**', redirectTo: 'allemployee', pathMatch: 'full' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PerformanceRoutingModule {}
--- src/app/pages/performance/performance.module.ts
+++ src/app/pages/performance/performance.module.ts
@@ -3,19 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { PerformanceComponent } from './performance.component';
 import { ModalsModule, WidgetsModule } from '../../_metronic/partials';
+import { DataTablesModule } from 'angular-datatables';
+import { AllEmployeeComponent } from './all-employees/all-employee.component';
+import { PerformanceRoutingModule } from './performance-routing.module';
+import { ProbationEmployeeComponent } from './probation-employees/probation-employee.component';
+import { ConfirmedEmployeeComponent } from './confirmed-employees/confirmed-employee.component';
+import { ExitedEmployeeComponent } from './exited-employees/exited-employee.component';
 
 
 
 @NgModule({
-  declarations: [PerformanceComponent],
+  declarations: [PerformanceComponent,AllEmployeeComponent,ProbationEmployeeComponent,ConfirmedEmployeeComponent,ExitedEmployeeComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: PerformanceComponent,
-      },
-    ]),
+    DataTablesModule,
+    PerformanceRoutingModule,
     WidgetsModule,
     ModalsModule,
   ]
